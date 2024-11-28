bb66df826ab8a06ff59b7bee66a56b0c2aed9ed9
sharifuzzamanShaon <shasan2200@gmail.com>
2024-11-28 13:25:29 +0600

    navbar config

--- src/utils/NavItems.jsx
+++ src/utils/NavItems.jsx
@@ -22,23 +22,24 @@ const navItemsData = [
 const NavItems = ({ activeItem, isMobile }) => {
   return (
     <>
-      <div className="hidden lg:flex">
-        {navItemsData?.map((i, index) => {
-          return (
-            <Link href={`${i.url}`} key={index} passHref>
+        {/* Navigation for larger screens */}
+        <div className=" md:hidden sm:hidden items-center space-x-8 pl-10  py-4">
+          {navItemsData?.map((i, index) => (
+            <Link href={i.url} key={index} passHref>
               <span
                 className={`${
                   activeItem === index
-                    ? "dark:text-[#37a39a] text-[#554fa7]"
-                    : "dark:text-white text-black"
-                } text-[18px] px-6 font-Poppins font-[400]`}
+                    ? "dark:text-[#37a39a] text-[#554fa7] font-semibold"
+                    : "dark:text-gray-300 text-gray-600"
+                } hover:dark:text-[#37a39a] hover:text-[#554fa7] transition-all duration-300 text-[18px] font-Poppins cursor-pointer px-2`}
               >
                 {i.name}
               </span>
             </Link>
-          );
-        })}
-      </div>
+          ))}
+        </div>
+    
+
       {isMobile && (
         <div className="lg:hidden md:hidden mt-5">
           {navItemsData &&
