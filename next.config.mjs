41950a330e20088f5e56aba8f6841457c21edc3f
sharifuzzamanShaon <shasan2200@gmail.com>
2024-12-03 19:20:05 +0600

    config next js for store persist data on the localstorage

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,19 @@ const nextConfig = {
       ? process.env.NEXT_PUBLIC_IMAGE_DOMAINS.split(",")
       : [],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Apply headers to all routes
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; connect-src 'self' https://skillsage-6v3g.onrender.com; script-src 'self' 'unsafe-inline'; img-src 'self' data: https:; style-src 'self' 'unsafe-inline'; object-src 'none'; frame-ancestors 'self';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
