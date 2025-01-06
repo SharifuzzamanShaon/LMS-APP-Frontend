44d4cebfa6ce904f283ad379ed2c35a0044de390
sharifuzzamanShaon <shasan2200@gmail.com>
2025-01-06 17:03:22 +0600

    enrollement api req

---
+++ redux/features/enrollCourse/enrollApi.js
@@ -0,0 +1,9 @@
+"use client"
+import { apiSlice } from "../api/apiSlice"
+
+export const enrollApi = apiSlice.injectEndpoints({
+    endpoints: (builder)=>({
+        
+    })
+})
+const {} = enrollApi
