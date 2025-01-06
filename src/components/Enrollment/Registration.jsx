94ea624af4d99be0543cd00c9b5103ba412025a7
sharifuzzamanShaon <shasan2200@gmail.com>
2025-01-06 17:12:16 +0600

    reg. to the course

--- src/components/Enrollment/Registration.jsx
+++ src/components/Enrollment/Registration.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import {
@@ -10,12 +10,17 @@ import {
   Box,
   Typography,
 } from "@mui/material";
+import { useSelector } from "react-redux";
+import { useLoginMutation, useRegisterMutation } from "../../../redux/features/auth/authApi";
+import toast from "react-hot-toast";
+import VerifyModalForEnroll from "@/utils/VerifyModalForEnroll";
 
 const Schema = Yup.object().shape({
   fullName: Yup.string().required("Full Name is required"),
   email: Yup.string()
     .email("Invalid email format")
     .required("Email is required"),
+  username: Yup.string().required("Username is required"),
   phoneNumber: Yup.string()
     .matches(/^[0-9]{10,15}$/, "Phone number must be between 10 and 15 digits")
     .required("Phone number is required"),
@@ -24,19 +29,63 @@ const Schema = Yup.object().shape({
     .required("Password is required"),
 });
 
-const Registration = () => {
+const Registration = ({ isVerified, setIsVerified }) => {
+  const [open, setOpen] = useState(false);
+  const { user } = useSelector((state) => state.auth);
+  const [register, { isError, data, error, isSuccess, isLoading }] =
+    useRegisterMutation();
+  const [
+    login,
+    { isSuccess: loginSccess, error: loginError, isLoading: loginiForLogin },
+  ] = useLoginMutation();
+  let loadingToast;
+  useEffect(() => {
+    if (isLoading) {
+      loadingToast = toast.loading("Signing up...");
+    }
+    if (isSuccess) {
+      toast.dismiss(loadingToast);
+      const msg =
+        data?.message ||
+        "Registration Successful | Please Verify your Email via OTP";
+      toast.success(msg);
+      setOpen(true);
+    }
+    if (error) {
+      toast.dismiss(loadingToast);
+      if ("data" in error) {
+        const errData = error;
+        toast.error(errData?.data.message);
+      } else {
+        toast.error("Something went wrong, try again");
+      }
+    }
+  }, [isLoading, isSuccess, error]);
+
+  useEffect(() => {
+    handleLogin()
+  }, [isVerified]);
+  const handleLogin = async () => {
+    console.log(values)   
+    const {email, password} = values 
+    await login({email, password});
+  };
   const formik = useFormik({
     initialValues: {
-      fullName: "",
-      email: "",
-      phoneNumber: "",
-      password: "",
+      fullName: "yilex84643@chansd.com",
+      email: user?.email ? user.email : "yilex84643@chansd.com",
+      username: user?.username ? user.username : "yilex84643@chansd.com",
+      phoneNumber: "12345678999",
+      password: user ? "*******" : "12341234",
     },
     validationSchema: Schema,
     onSubmit: async (values, { setSubmitting }) => {
+      const { email, username, password } = values;
       try {
+        const regValue = { email, username, password };
+        console.log(":::", regValue);
+        await register(regValue);
         console.log("Form data submitted:", values);
-        alert("Registration Successful");
       } catch (error) {
         console.error("Error during submission:", error);
         alert("An unexpected error occurred. Please try again.");
@@ -45,18 +94,26 @@ const Registration = () => {
       }
     },
   });
-
-  const { errors, touched, values, handleChange, handleSubmit } = formik;
+  const { errors, touched, values, handleChange, handleSubmit, resetForm } =
+    formik;
+  useEffect(() => {
+    if (isVerified) {
+      resetForm();
+    }
+  }, [isVerified, resetForm]);
 
   return (
-    <div
-    >
+    <div className="dark:bg-gray-900 min-[600v] flex items-center justify-center p-4">
       <Box
         component="form"
         onSubmit={handleSubmit}
-        className="max-w-3xl w-full p-6 bg-white rounded-lg shadow-lg"
+        className="max-w-3xl w-full p-6 bg-white dark:bg-gray-800 rounded-lg shadow-lg"
       >
-        <Typography variant="h5" mb={3} align="center">
+        <Typography
+          variant="h5"
+          align="center"
+          className="text-gray-900 dark:text-gray-100 mb-6"
+        >
           Registration
         </Typography>
 
@@ -65,18 +122,23 @@ const Registration = () => {
           {/* Full Name */}
           <div>
             <FormControl fullWidth variant="outlined" className="mb-4">
-              <InputLabel htmlFor="fullName">Full Name</InputLabel>
+              <InputLabel htmlFor="fullName" className="dark:text-gray-400">
+                Full Name
+              </InputLabel>
               <Input
                 id="fullName"
                 name="fullName"
                 value={values.fullName}
                 onChange={handleChange}
+                className="dark:text-gray-100"
               />
               <FormHelperText>
                 {errors.fullName && touched.fullName ? (
                   <span className="text-red-500">{errors.fullName}</span>
                 ) : (
-                  "Enter your full name"
+                  <span className="dark:text-gray-400">
+                    Enter your full name
+                  </span>
                 )}
               </FormHelperText>
             </FormControl>
@@ -84,41 +146,83 @@ const Registration = () => {
 
           {/* Email */}
           <div>
-            <FormControl fullWidth variant="outlined" className="mb-4">
-              <InputLabel htmlFor="email">Email</InputLabel>
+            <FormControl
+              disabled={user}
+              fullWidth
+              variant="outlined"
+              className="mb-4"
+            >
+              <InputLabel htmlFor="email" className="dark:text-gray-400">
+                Email
+              </InputLabel>
               <Input
                 id="email"
                 name="email"
                 type="email"
                 value={values.email}
                 onChange={handleChange}
+                className="dark:text-gray-100"
               />
               <FormHelperText>
                 {errors.email && touched.email ? (
                   <span className="text-red-500">{errors.email}</span>
                 ) : (
-                  "Enter a valid email address"
+                  <span className="dark:text-gray-400">
+                    Enter a valid email address
+                  </span>
                 )}
               </FormHelperText>
             </FormControl>
           </div>
-
+          {/* username */}
+          <div>
+            <FormControl
+              disabled={user}
+              fullWidth
+              variant="outlined"
+              className="mb-4"
+            >
+              <InputLabel htmlFor="username" className="dark:text-gray-400">
+                username
+              </InputLabel>
+              <Input
+                id="username"
+                name="username"
+                type="text"
+                value={values.username}
+                onChange={handleChange}
+                className="dark:text-gray-100"
+              />
+              <FormHelperText>
+                {errors.username && touched.username ? (
+                  <span className="text-red-500">{errors.username}</span>
+                ) : (
+                  <span className="dark:text-gray-400">Enter a username</span>
+                )}
+              </FormHelperText>
+            </FormControl>
+          </div>
           {/* Phone Number */}
           <div>
             <FormControl fullWidth variant="outlined" className="mb-4">
-              <InputLabel htmlFor="phoneNumber">Phone Number</InputLabel>
+              <InputLabel htmlFor="phoneNumber" className="dark:text-gray-400">
+                Phone Number
+              </InputLabel>
               <Input
                 id="phoneNumber"
                 name="phoneNumber"
                 type="text"
                 value={values.phoneNumber}
                 onChange={handleChange}
+                className="dark:text-gray-100"
               />
               <FormHelperText>
                 {errors.phoneNumber && touched.phoneNumber ? (
                   <span className="text-red-500">{errors.phoneNumber}</span>
                 ) : (
-                  "Enter your phone number"
+                  <span className="dark:text-gray-400">
+                    Enter your phone number
+                  </span>
                 )}
               </FormHelperText>
             </FormControl>
@@ -126,36 +230,52 @@ const Registration = () => {
 
           {/* Password */}
           <div>
-            <FormControl fullWidth variant="outlined" className="mb-4">
-              <InputLabel htmlFor="password">Password</InputLabel>
+            <FormControl
+              disabled={user}
+              fullWidth
+              variant="outlined"
+              className="mb-4"
+            >
+              <InputLabel htmlFor="password" className="dark:text-gray-400">
+                Password
+              </InputLabel>
               <Input
                 id="password"
                 name="password"
                 type="password"
                 value={values.password}
                 onChange={handleChange}
+                className="dark:text-gray-100"
               />
               <FormHelperText>
                 {errors.password && touched.password ? (
                   <span className="text-red-500">{errors.password}</span>
                 ) : (
-                  "Enter a strong password"
+                  <span className="dark:text-gray-400">
+                    Enter a strong password
+                  </span>
                 )}
               </FormHelperText>
             </FormControl>
           </div>
         </div>
-
         {/* Submit Button */}
         <Button
           type="submit"
           variant="contained"
           color="primary"
-          className="mt-8"
+          className="mt-12 dark:bg-blue-600 dark:text-white"
         >
           Submit
         </Button>
       </Box>
+      {open && (
+        <VerifyModalForEnroll
+          open={open}
+          setOpen={setOpen}
+          setIsVerified={setIsVerified}
+        />
+      )}
     </div>
   );
 };
