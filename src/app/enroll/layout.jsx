1372f1b55d50b525b86439e72243fc3a39054f5f
sharifuzzamanShaon <shasan2200@gmail.com>
2025-02-10 20:59:41 +0600

    bg color

--- src/app/enroll/layout.jsx
+++ src/app/enroll/layout.jsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }) {
   const [open, setOpen] = useState(false);
   const [activeItem, setActiveItem] = useState(0);
   return (
-    <div className="h-[800px]">
+    <div className="overflow-hidden height-screen bg-no-repeat dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300">
       <Header open={open} activeItem={activeItem} setOpen={setOpen}></Header>
       {children}
     </div>
