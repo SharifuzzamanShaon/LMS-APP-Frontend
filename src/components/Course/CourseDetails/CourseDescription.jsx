082081bed5728bdaee23ce3b2ccabd48744b08da
sharifuzzamanShaon <shasan2200@gmail.com>
2025-02-12 21:00:19 +0600

    show course info

--- src/components/Course/CourseDetails/CourseDescription.jsx
+++ src/components/Course/CourseDetails/CourseDescription.jsx
@@ -62,7 +62,6 @@ const CourseDescription = ({ course }) => {
           ))}
         </ul>
       </div>
-
       {/* Course Data Sections */}
       <div className="mb-6">
         <h2 className="text-lg font-semibold text-gray-800 dark:text-white mb-2">
