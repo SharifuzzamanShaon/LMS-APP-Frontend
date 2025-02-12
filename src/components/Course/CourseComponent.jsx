8fc710a171bff8178b5bb773ca2e50b6408b4439
sharifuzzamanShaon <shasan2200@gmail.com>
2025-02-12 20:59:53 +0600

    change the design

--- src/components/Course/CourseComponent.jsx
+++ src/components/Course/CourseComponent.jsx
@@ -7,36 +7,36 @@ import FilterCourseByCategoryBtn from "../Filter-components/FilterCourseByCatego
 
 const CourseCard = ({ course }) => {
   return (
-    <div className="bg-white dark:bg-gray-800 dark:text-gray-200 shadow-md rounded-lg p-4 transition duration-300 flex flex-col h-full">
-      {course.thumbnail && (
-        <div className="mb-4 relative w-full h-48">
-          <Image
-            src={course.thumbnail.url}
-            alt={`${course.name} Thumbnail`}
-            layout="fill"
-            objectFit="cover"
-            className="rounded-md"
-          />
-        </div>
-      )}
-      <h2 className="text-sm font-bold text-gray-700 dark:text-gray-400 mb-2">
-        {course.name}
-      </h2>
-      <p className="text-xs text-gray-700 dark:text-gray-400 mb-2 line-clamp-2">
-        {course.description || "No description available."}
-      </p>
-      <p className="text-sm text-gray-700 dark:text-gray-400 mb-2">
-        <strong>Price:</strong> ${course.price}
-      </p>
-      <p className="text-xs text-gray-600 dark:text-gray-400">
-        <strong>Level:</strong> {course.level.join(", ")}
-      </p>
-      <Link href={`/courses/${course._id}`}>
-        <button className="w-full bg-blue-500 dark:bg-blue-700 text-white py-1 px-2 text-sm rounded-md hover:bg-blue-600 dark:hover:bg-blue-800 transition">
-          View Details
-        </button>
-      </Link>
-    </div>
+      <div className="bg-white dark:bg-gray-800 dark:text-gray-200 shadow-md rounded-lg p-4 transition duration-300 flex flex-col h-full">
+        {course.thumbnail && (
+          <div className="mb-4 relative w-full h-48">
+            <Image
+              src={course.thumbnail.url}
+              alt={`${course.name} Thumbnail`}
+              layout="fill"
+              objectFit="cover"
+              className="rounded-md"
+            />
+          </div>
+        )}
+        <h2 className="text-sm font-bold text-gray-700 dark:text-gray-400 mb-2">
+          {course.name}
+        </h2>
+        <p className="text-xs text-gray-700 dark:text-gray-400 mb-2 line-clamp-2">
+          {course.description || "No description available."}
+        </p>
+        <p className="text-sm text-gray-700 dark:text-gray-400 mb-2">
+          <strong>Price:</strong> ${course.price}
+        </p>
+        <p className="text-xs text-gray-600 dark:text-gray-400">
+          <strong>Level:</strong> {course.level.join(", ")}
+        </p>
+        <Link href={`/courses/${course._id}`}>
+          <button className="w-full bg-blue-500 dark:bg-blue-700 text-white py-1 px-2 text-sm rounded-md hover:bg-blue-600 dark:hover:bg-blue-800 transition">
+            View Details
+          </button>
+        </Link>
+      </div>
   );
 };
 
@@ -73,7 +73,7 @@ const CourseComponent = () => {
       <FilterCourseByCategoryBtn/>
       {errorMessage && <p className="text-red-500">{errorMessage}</p>}
       {courses?.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 py-5">
           {courses.map((course) => (
             <CourseCard key={course.id} course={course} />
           ))}
