ac54a81705dc0bf575d615ab0ce8b8a45ea1369b
sharifuzzamanShaon <shasan2200@gmail.com>
2025-02-12 21:03:07 +0600

    show time of msg

--- src/components/StudentLounge/ChatArea/MessageFromOutside.jsx
+++ src/components/StudentLounge/ChatArea/MessageFromOutside.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-import "../../../globals.css"
+import "../../../globals.css";
 
-const MessageFromOutside = ({props}) => {
+const MessageFromOutside = ({ props }) => {
+  console.log(props);
+  const { createdAt, content, sender } = props;
+  let date = new Date(createdAt);
   return (
     <div className={"other-message-container"}>
       <div className={"conversation-container"}>
-        <p className={"con-icon" }>
-            {/* {props?.sender.username[0]} */}
-            {Array.isArray(props?.sender.username) ? (props?.sender.username[0])  : (props?.sender.username)}
+        <p className={"con-icon"}>
+          {/* {props?.sender.username[0]} */}
+          {Array.isArray(sender.username)
+            ? sender.username[0]
+            : sender.username}
         </p>
         <div className={"other-text-content flex flex-col"}>
           {/* <p className={"con-title"}>{props.sender.name}</p> */}
-          <p className="con-lastMessage font-bold  text-white">{props.content}</p>
-          <p className="self-timeStamp">12:00am</p>
+          <p className="con-lastMessage font-bold  text-white">{content}</p>
+            <p className="self-timeStamp">{date.toLocaleTimeString()}</p>
         </div>
       </div>
     </div>
