3d8e2eeea092feecc17f01b5351a6a4e73087736
sharifuzzamanShaon <shasan2200@gmail.com>
2025-02-13 23:26:19 +0600

    required auth. for chats

--- src/utils/NavItems.jsx
+++ src/utils/NavItems.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import { useSelector } from "react-redux";
+import { toast } from "react-hot-toast";
 
 const navItemsData = [
   {
@@ -20,15 +21,20 @@ const navItemsData = [
 
 const NavItems = ({ activeItem, isMobile }) => {
   const { user } = useSelector((state) => state.auth);
-  const filteredNavItems = navItemsData.filter(
-    (item) => !item.requiresAuth || user
-  );
+
+  const handleNavClick = (e, item) => {
+    if (item.requiresAuth && !user) {
+      e.preventDefault();
+      toast.error("Please login to access this feature");
+    }
+  };
+
   return (
     <>
       {/* Navigation for larger screens */}
       <div className="hidden lg:block space-x-8 pl-10 p-4 ">
-        {filteredNavItems?.map((i, index) => (
-          <Link href={i.url} key={index} passHref>
+        {navItemsData?.map((i, index) => (
+          <Link href={i.url} key={index} passHref onClick={(e) => handleNavClick(e, i)}>
             <span
               className={`
                     "dark:text-gray-300 text-gray-600 hover:dark:text-[#37a39a] hover:text-[#554fa7] transition-all duration-300 text-[18px] font-Poppins cursor-pointer px-2`}
@@ -41,14 +47,14 @@ const NavItems = ({ activeItem, isMobile }) => {
       {/* Navigation for smaller screens */}
       {isMobile && (
         <div className="lg:hidden mt-5 pl-4">
-          {filteredNavItems.map((i, index) => (
-            <Link href={i.url} key={index} passHref>
+          {navItemsData.map((i, index) => (
+            <Link href={i.url} key={index} passHref onClick={(e) => handleNavClick(e, i)}>
               <span
                 className={`${
                   activeItem === index
                     ? "dark:text-[#37a39a] text-[crimson]"
                     : "dark:text-white text-black"
-                } block py-5 text-[18px] px-6  font-Poppins font-[400]`}
+                } block py-2 text-[18px] px-6  font-Poppins font-[400]`}
               >
                 {i.name}
               </span>
