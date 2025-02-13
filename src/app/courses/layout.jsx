db1239199020bc88e87109fd4724dfe58ce287ec
sharifuzzamanShaon <shasan2200@gmail.com>
2025-02-13 23:17:56 +0600

    layout height

--- src/app/courses/layout.jsx
+++ src/app/courses/layout.jsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }) {
   const [open, setOpen] = useState(false);
   const [activeItem, setActiveItem] = useState(0);
   return (
-    <div className="h-[500px]">
+    <div className="h-screen">
       <Header open={open} activeItem={activeItem} setOpen={setOpen}></Header>
       {children}
     </div>
