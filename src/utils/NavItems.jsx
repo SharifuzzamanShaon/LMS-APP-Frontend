180c40dfa1d238870dad088091f394373c14824a
sharifuzzamanShaon <shasan2200@gmail.com>
2025-02-15 12:53:47 +0600

    redesign the navitems

--- src/utils/NavItems.jsx
+++ src/utils/NavItems.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import { useSelector } from "react-redux";
 import { toast } from "react-hot-toast";
+import { Button } from "@mui/material";
 
 const navItemsData = [
   {
@@ -12,18 +13,14 @@ const navItemsData = [
     name: "Courses",
     url: "/courses",
   },
-  {
-    name: "Chat's",
-    url: "/student-lounge",
-    requiresAuth: true,
-  }
+
 ];
 
 const NavItems = ({ activeItem, isMobile }) => {
   const { user } = useSelector((state) => state.auth);
 
-  const handleNavClick = (e, item) => {
-    if (item.requiresAuth && !user) {
+  const handleChatClick = (e) => {
+    if (!user) {
       e.preventDefault();
       toast.error("Please login to access this feature");
     }
@@ -33,33 +30,53 @@ const NavItems = ({ activeItem, isMobile }) => {
     <>
       {/* Navigation for larger screens */}
       <div className="hidden lg:block space-x-8 pl-10 p-4 ">
-        {navItemsData?.map((i, index) => (
-          <Link href={i.url} key={index} passHref onClick={(e) => handleNavClick(e, i)}>
-            <span
-              className={`
-                    "dark:text-gray-300 text-gray-600 hover:dark:text-[#37a39a] hover:text-[#554fa7] transition-all duration-300 text-[18px] font-Poppins cursor-pointer px-2`}
+        <Link href="/" passHref>
+          <span className="dark:text-gray-300 text-gray-600 hover:dark:text-[#37a39a] hover:text-[#554fa7] transition-all duration-300 text-[18px] font-Poppins cursor-pointer px-2">
+            Home
+          </span>
+        </Link>
+        <Link href="/courses" passHref>
+          <span className="dark:text-gray-300 text-gray-600 hover:dark:text-[#37a39a] hover:text-[#554fa7] transition-all duration-300 text-[18px] font-Poppins cursor-pointer px-2">
+            Courses
+          </span>
+        </Link>
+     
+          <Link href="/student-lounge" onClick={handleChatClick}>
+            <Button
+              sx={{
+                backgroundColor: '#37a39a',
+                color: 'white',
+                '&:hover': {
+                  backgroundColor: '#2c827a',
+                },
+              }}
             >
-              {i.name}
-            </span>
+              CHAT's
+            </Button>
           </Link>
-        ))}
       </div>
+
       {/* Navigation for smaller screens */}
       {isMobile && (
         <div className="lg:hidden mt-5 pl-4">
-          {navItemsData.map((i, index) => (
-            <Link href={i.url} key={index} passHref onClick={(e) => handleNavClick(e, i)}>
-              <span
-                className={`${
-                  activeItem === index
-                    ? "dark:text-[#37a39a] text-[crimson]"
-                    : "dark:text-white text-black"
-                } block py-2 text-[18px] px-6  font-Poppins font-[400]`}
-              >
-                {i.name}
-              </span>
-            </Link>
-          ))}
+          <Link href="/" passHref>
+            <span className={`${
+              activeItem === 0
+                ? "dark:text-[#37a39a] text-[crimson]"
+                : "dark:text-white text-black"
+            } block py-2 text-[18px] px-6 font-Poppins font-[400]`}>
+              Home
+            </span>
+          </Link>
+          <Link href="/courses" passHref>
+            <span className={`${
+              activeItem === 1
+                ? "dark:text-[#37a39a] text-[crimson]"
+                : "dark:text-white text-black"
+            } block py-2 text-[18px] px-6 font-Poppins font-[400]`}>
+              Courses
+            </span>
+          </Link>
         </div>
       )}
     </>
