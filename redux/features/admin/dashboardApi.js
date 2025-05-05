8e3dc6ea781bc2b6ed07ca05c4b230b29c7815d4
sharifuzzamanshaon <shasan2200@gmail.com>
2025-05-05 18:48:15 +0600

    Refactor adminDashboardApi endpoints for improved readability and consistency

--- redux/features/admin/dashboardApi.js
+++ redux/features/admin/dashboardApi.js
@@ -2,60 +2,66 @@
 import { apiSlice } from "../api/apiSlice";
 
 export const adminDashboardApi = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        getDashboardInfo: builder.query({
-            query:()=>({
-                url: "/admin/dashboard",
-                credentials: "include",
-                method: "GET",
-            }),
-            async onQueryStarted(arg, { queryFulfilled, dispatch }) {
-                try {
-                    const res = await queryFulfilled;
-                    console.log(res);
-                    // dispatch(setDashboardInfo(res.data));
-                    return res;
-                } catch (error) {
-                    console.log(error);
-                }   
-            }
-        }),
-        getStudentList: builder.query({
-            query:()=>({
-                url: "/admin/students",
-                credentials: "include",
-                method: "GET",
-            }),
-        }),
-        getCourseList: builder.query({
-            query:()=>({
-                url: "/admin/courses",
-                credentials: "include",
-                method: "GET",
-            }),
-            async onQueryStarted(arg, { queryFulfilled, dispatch }) {
-                try {
-                    const res = await queryFulfilled;
-                    return res?.data?.courses;
-                } catch (error) {
-                    console.log(error);
-                }
-            }
-        }),
-        editCourse: builder.mutation({
-            query:({id, data})=>({
-                url: `/admin/courses/${id}`,
-                credentials: "include",
-                method: "PUT",
-            }),
-        }),
-        getCourseById: builder.query({
-            query:({id})=>({
-                url: `/admin/courses/${id}`,
-                credentials: "include",
-                method: "GET",
-            }),
-        })
-    })
-})
-export const {  useGetDashboardInfoQuery, useGetStudentListQuery, useGetCourseListQuery, useEditCourseMutation, useGetCourseByIdQuery } = adminDashboardApi;
+  endpoints: (builder) => ({
+    getDashboardInfo: builder.query({
+      query: () => ({
+        url: "/admin/dashboard",
+        credentials: "include",
+        method: "GET",
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const res = await queryFulfilled;
+          console.log(res);
+          // dispatch(setDashboardInfo(res.data));
+          return res;
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }),
+    getStudentList: builder.query({
+      query: () => ({
+        url: "/admin/students",
+        credentials: "include",
+        method: "GET",
+      }),
+    }),
+    getCourseList: builder.query({
+      query: () => ({
+        url: "/admin/courses",
+        credentials: "include",
+        method: "GET",
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const res = await queryFulfilled;
+          return res?.data?.courses;
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }),
+    editCourse: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/admin/courses/${id}`,
+        credentials: "include",
+        method: "PUT",
+      }),
+    }),
+    getCourseById: builder.query({
+      query: ({ id }) => ({
+        url: `/admin/courses/${id}`,
+        credentials: "include",
+        method: "GET",
+      }),
+    }),
+  }),
+});
+export const {
+  useGetDashboardInfoQuery,
+  useGetStudentListQuery,
+  useGetCourseListQuery,
+  useEditCourseMutation,
+  useGetCourseByIdQuery,
+} = adminDashboardApi;
